778a583c1201cde91a1a9d271131acbba53cff8c
yjlin03 <yjlin03@mail.nfsq.com.cn>
2017-07-13 16:24:40 +0800

    增加drui监控配置

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication//相当于@Configuration,@EnableAutoConfiguration,@ComponentScan
+@ServletComponentScan//扫描原生Servlet3.0的注解进行配置、@WebServlet 、@WebListener、@WebFilte
 @EnableTransactionManagement
 public class DemoApplication {
 
--- src/main/java/com/example/demo/common/MybatisConfig.java
+++ src/main/java/com/example/demo/common/MybatisConfig.java
@@ -1,16 +1,22 @@
 package com.example.demo.common;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.pool.DruidDataSourceFactory;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 import java.util.Properties;
 
 /**
@@ -22,8 +28,63 @@ import java.util.Properties;
 @Configuration
 @MapperScan(basePackages="com.example.demo.dao.mapper")
 public class MybatisConfig {
+    private Logger logger = LoggerFactory.getLogger(MybatisConfig.class);
     @Autowired
     private Environment env;
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driverClassName}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.initialSize}")
+    private int initialSize;
+
+    @Value("${spring.datasource.minIdle}")
+    private int minIdle;
+
+    @Value("${spring.datasource.maxActive}")
+    private int maxActive;
+
+    @Value("${spring.datasource.maxWait}")
+    private int maxWait;
+
+    @Value("${spring.datasource.timeBetweenEvictionRunsMillis}")
+    private int timeBetweenEvictionRunsMillis;
+
+    @Value("${spring.datasource.minEvictableIdleTimeMillis}")
+    private int minEvictableIdleTimeMillis;
+
+    @Value("${spring.datasource.validationQuery}")
+    private String validationQuery;
+
+    @Value("${spring.datasource.testWhileIdle}")
+    private boolean testWhileIdle;
+
+    @Value("${spring.datasource.testOnBorrow}")
+    private boolean testOnBorrow;
+
+    @Value("${spring.datasource.testOnReturn}")
+    private boolean testOnReturn;
+
+    @Value("${spring.datasource.poolPreparedStatements}")
+    private boolean poolPreparedStatements;
+
+    @Value("${spring.datasource.maxPoolPreparedStatementPerConnectionSize}")
+    private int maxPoolPreparedStatementPerConnectionSize;
+
+    @Value("${spring.datasource.filters}")
+    private String filters;
+
+    @Value("{spring.datasource.connectionProperties}")
+    private String connectionProperties;
 
     /**
      * 创建数据源
@@ -31,15 +92,46 @@ public class MybatisConfig {
      * @throws Exception
      */
     //@Bean
-    @Bean(name="testDataSource", destroyMethod = "close", initMethod="init")
-    //@Primary  @Primary 该注解表示在同一个接口有多个实现类可以注入的时候，默认选择哪一个，而不是让@autowire注解报错
-    public DataSource getDataSource() throws Exception{
-        Properties props = new Properties();
-        props.put("driverClassName", env.getProperty("jdbc.driverClassName"));
-        props.put("url", env.getProperty("jdbc.url"));
-        props.put("username", env.getProperty("jdbc.username"));
-        props.put("password", env.getProperty("jdbc.password"));
-        return DruidDataSourceFactory.createDataSource(props);
+//    @Bean(name="testDataSource", destroyMethod = "close", initMethod="init")
+//    //@Primary  @Primary 该注解表示在同一个接口有多个实现类可以注入的时候，默认选择哪一个，而不是让@autowire注解报错
+//    public DataSource getDataSource() throws Exception{
+//        Properties props = new Properties();
+//        props.put("driverClassName", env.getProperty("jdbc.driverClassName"));
+//        props.put("url", env.getProperty("jdbc.url"));
+//        props.put("username", env.getProperty("jdbc.username"));
+//        props.put("password", env.getProperty("jdbc.password"));
+//        return DruidDataSourceFactory.createDataSource(props);
+//    }
+    @Bean(name="DataSource2", destroyMethod = "close", initMethod="init")
+    @Primary//表示在同一个接口有多个实现类可以注入的时候，默认选择
+    public DataSource getDataSourceTwo()throws Exception{
+        DruidDataSource datasource = new DruidDataSource();
+        datasource.setUrl(this.dbUrl);
+        datasource.setUsername(username);
+        datasource.setPassword(password);
+        datasource.setDriverClassName(driverClassName);
+
+        //configuration
+        datasource.setInitialSize(initialSize);
+        datasource.setMinIdle(minIdle);
+        datasource.setMaxActive(maxActive);
+        datasource.setMaxWait(maxWait);
+        datasource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        datasource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        datasource.setValidationQuery(validationQuery);
+        datasource.setTestWhileIdle(testWhileIdle);
+        datasource.setTestOnBorrow(testOnBorrow);
+        datasource.setTestOnReturn(testOnReturn);
+        datasource.setPoolPreparedStatements(poolPreparedStatements);
+        datasource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
+        try {
+            datasource.setFilters(filters);
+        } catch (SQLException e) {
+            logger.error("druid configuration initialization filter", e);
+        }
+        datasource.setConnectionProperties(connectionProperties);
+
+        return datasource;
     }
     /**
        * 根据数据源创建SqlSessionFactory
